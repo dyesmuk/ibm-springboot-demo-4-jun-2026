6de9603ee483219923ab6839f352eafb65dc297e
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-06-16 15:01:02 +0530

    assignment solved

--- src/main/java/com/ibm/demo/model/Employee.java
+++ src/main/java/com/ibm/demo/model/Employee.java
@@ -1,3 +1,17 @@
+//EmployeeRequestDTO - client sends  
+//EmployeeResponseDTO - server returns 
+//Employee - model class  
+//
+//EmployeeMapper class
+//
+//EmployeeService 
+//EmployeeServiceImpl  
+//
+//EmployeeController
+
+
+
+
 package com.ibm.demo.model;
 
 import jakarta.validation.constraints.Email;
@@ -96,3 +110,105 @@ public class Employee {
 	}
 
 }
+
+
+
+
+//package com.ibm.demo.model;
+//
+//import jakarta.validation.constraints.Email;
+//import jakarta.validation.constraints.NotBlank;
+//import jakarta.validation.constraints.NotNull;
+//import jakarta.validation.constraints.Positive;
+//import jakarta.validation.constraints.Size;
+//
+//import org.springframework.data.annotation.Id;
+//import org.springframework.data.mongodb.core.index.Indexed;
+//import org.springframework.data.mongodb.core.mapping.Document;
+//import org.springframework.data.mongodb.core.mapping.Field;
+//
+////validations constraints  
+////https://jakarta.ee/specifications/bean-validation/3.0/apidocs/jakarta/validation/constraints/package-summary
+//
+//@Document(collection = "employees")
+//public class Employee {
+//
+//	@Id
+//	private String id;
+//
+//	@NotBlank(message = "First name must not be blank")
+//	@Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
+//	@Field("first_name")
+//	private String firstName;
+//
+//	@NotBlank(message = "Last name must not be blank")
+//	@Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
+//	@Field("last_name")
+//	private String lastName;
+//
+//	@NotBlank(message = "Email must not be blank")
+//	@Email(message = "Email must be a valid email address")
+//	@Indexed(unique = true)
+//	private String email;
+//
+//	@NotNull(message = "Salary must not be null")
+//	@Positive(message = "Salary must be a positive value")
+//	private double salary;
+//
+//	public Employee() {
+//	}
+//
+//	public Employee(String firstName, String lastName, String email, double salary) {
+//		this.firstName = firstName;
+//		this.lastName = lastName;
+//		this.email = email;
+//		this.salary = salary;
+//	}
+//
+//	public String getId() {
+//		return id;
+//	}
+//
+//	public void setId(String id) {
+//		this.id = id;
+//	}
+//
+//	public String getFirstName() {
+//		return firstName;
+//	}
+//
+//	public void setFirstName(String firstName) {
+//		this.firstName = firstName;
+//	}
+//
+//	public String getLastName() {
+//		return lastName;
+//	}
+//
+//	public void setLastName(String lastName) {
+//		this.lastName = lastName;
+//	}
+//
+//	public String getEmail() {
+//		return email;
+//	}
+//
+//	public void setEmail(String email) {
+//		this.email = email;
+//	}
+//
+//	public double getSalary() {
+//		return salary;
+//	}
+//
+//	public void setSalary(double salary) {
+//		this.salary = salary;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+//				+ ", salary=" + salary + "]";
+//	}
+//
+//}
